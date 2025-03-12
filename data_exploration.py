fa96625768dc73d2cd08d1b168bdd29fac64cc16
Nhan Le <nhanle0829.work@gmail.com>
2025-03-11 22:00:10 -0700

    plot pixel distribution

--- data_exploration.py
+++ data_exploration.py
@@ -15,26 +15,55 @@ def random_indexes_number(number, number_of_index=5):
     return indices
 
 # Samples for each digit
-fig, axes = plt.subplots(10, 5, figsize=(20, 10))
+def plat_samples_for_each_digit():
+    fig, axes = plt.subplots(10, 5, figsize=(20, 10))
 
-for i in range(10):
-    random_indices = random_indexes_number(i)
-    for j in range(5):
-        img = X[:, :, :, random_indices[j]]
-        axes[i][j].imshow(img)
+    for i in range(10):
+        random_indices = random_indexes_number(i)
+        for j in range(5):
+            img = X[:, :, :, random_indices[j]]
+            axes[i][j].imshow(img)
 
-        axes[i][j].set_xticks([])
-        axes[i][j].set_yticks([])
+            axes[i][j].set_xticks([])
+            axes[i][j].set_yticks([])
 
-plt.suptitle("SVHN Dataset Samples for Each Digit", fontsize=30)
-plt.tight_layout()
-plt.show()
+    plt.suptitle("SVHN Dataset Samples for Each Digit", fontsize=30)
+    plt.tight_layout()
+    plt.show()
 
 # Plot class distribution
-unique, count = np.unique(y, return_counts=True)
-plt.bar(unique, count)
-plt.xlabel("Digit")
-plt.ylabel("Count")
-plt.title("Class Distribution in SVHN Dataset")
-plt.xticks(unique)
-plt.show()
+def plot_class_distribution():
+    unique, count = np.unique(y, return_counts=True)
+    plt.bar(unique, count)
+    plt.xlabel("Digit")
+    plt.ylabel("Count")
+    plt.title("Class Distribution in SVHN Dataset")
+    plt.xticks(unique)
+    plt.show()
+
+# Plot pixel distribution
+def plot_pixel_distribution(sample_size=1000):
+    X_reshaped = X.transpose(3, 0, 1, 2)
+    if len(X_reshaped[0] > sample_size):
+        indices = np.random.choice(X_reshaped.shape[0], size=sample_size, replace=False)
+        X_reshaped = X_reshaped[indices]
+
+    pixel_r = X_reshaped[:, :, :, 0].flatten()
+    pixel_g = X_reshaped[:, :, :, 1].flatten()
+    pixel_b = X_reshaped[:, :, :, 2].flatten()
+    pixel_list = [pixel_r, pixel_g, pixel_b]
+
+
+    fig, axes = plt.subplots(1, 3)
+    axes[0].hist(pixel_list[0], bins=50, color="r")
+    axes[1].hist(pixel_list[1], bins=50, color="g")
+    axes[2].hist(pixel_list[2], bins=50, color="b")
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    plat_samples_for_each_digit()
+    plot_class_distribution()
+    plot_pixel_distribution()
